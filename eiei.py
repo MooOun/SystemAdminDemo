4560bc034a8c9e5c2b4c0a743cafd7f644b2c4d3
Kanthapong <kanthapong062@gmail.com>
2023-02-20 11:16:21 +0700

    Create Function

--- eiei.py
+++ eiei.py
@@ -1,1 +1,12 @@
 print('eieie')
+
+def create_newblock(self,data):
+        self.blockNo = len(self.blockchainList)+1
+        self.nonce = random.randint(0,100)
+        self.previous_hash=self.blockchainList[-1]['hash']
+        self.timestamp=str(datetime.datetime.now())
+        self.transaction=data
+        self.hash=None
+
+        self.hashblock()
+        self.addToBlockkchain()
